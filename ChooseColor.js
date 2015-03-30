ee72578dd70cef294e733706ed4cfedee15390c7
Jake Irvin <jbirvin@asu.edu>
2015-03-29 21:22:40 -0700

    Here is all of the files for our Palliative Care App GUI

---
+++ ChooseColor.js
@@ -0,0 +1,42 @@
+﻿/*
+JS to change the color of the row based on patient condition
+*/
+var table;
+var row;
+var cell;
+var priority;
+
+function colorRow() {
+
+    var status;
+    table = document.getElementById("tableID");
+    row = table.getElementsByTagName("tr");
+    cell = table.getElementsByTagName("td");
+    
+    for (var i = 0; i < cell.length; i++)
+    {
+        if(cell[i].innerText == "Critical")
+        {
+            cell[i].parentNode.style.backgroundColor = "red";
+        }
+        if (cell[i].innerText == "Moderate")
+        {
+            cell[i].parentNode.style.backgroundColor = "yellow";
+        }
+    }
+}
+
+function colorPriority() {
+
+    priority = document.getElementById("priority");
+    
+    if (priority.innerText == "Critical")
+    {
+        priority.style.backgroundColor = "red";
+    }
+    if (priority.innerText == "Moderate")
+    {
+        priority.style.backgroundColor = "yellow";
+    }
+
+}
---
+++ SurveyScript.js
@@ -0,0 +1,477 @@
+﻿/*
+Javascript code for getting survey results
+*/
+
+// array with all the results
+var results = [0,0,0,0,0,0,0,0,0]
+
+function selectPain(selected)
+{
+    var a = document.getElementById("1.0");
+    var b = document.getElementById("1.1");
+    var c = document.getElementById("1.2");
+    var d = document.getElementById("1.3");
+    var e = document.getElementById("1.4");
+    var f = document.getElementById("1.5");
+    var g = document.getElementById("1.6");
+    var h = document.getElementById("1.7");
+    var i = document.getElementById("1.8");
+    var j = document.getElementById("1.9");
+    var k = document.getElementById("1.10");
+
+    a.className = "selectNumberDiv";
+    b.className = "selectNumberDiv";
+    c.className = "selectNumberDiv";
+    d.className = "selectNumberDiv";
+    e.className = "selectNumberDiv";
+    f.className = "selectNumberDiv";
+    g.className = "selectNumberDiv";
+    h.className = "selectNumberDiv";
+    i.className = "selectNumberDiv";
+    j.className = "selectNumberDiv";
+    k.className = "selectNumberDiv";
+    
+
+    if (selected.id == "1.0")
+        results[0] = 0;
+    if (selected.id == "1.1")
+        results[0] = 1;
+    if (selected.id == "1.2")
+        results[0] = 2;
+    if (selected.id == "1.3")
+        results[0] = 3;
+    if (selected.id == "1.4")
+        results[0] = 4;
+    if (selected.id == "1.5")
+        results[0] = 5;
+    if (selected.id == "1.6")
+        results[0] = 6;
+    if (selected.id == "1.7")
+        results[0] = 7;
+    if (selected.id == "1.8")
+        results[0] = 8;
+    if (selected.id == "1.9")
+        results[0] = 9;
+    if (selected.id == "1.10")
+        results[0] = 10;
+
+    selected.className = "selectedNumberDiv";
+}
+
+function selectTiredness(selected)
+{
+    var a = document.getElementById("2.0");
+    var b = document.getElementById("2.1");
+    var c = document.getElementById("2.2");
+    var d = document.getElementById("2.3");
+    var e = document.getElementById("2.4");
+    var f = document.getElementById("2.5");
+    var g = document.getElementById("2.6");
+    var h = document.getElementById("2.7");
+    var i = document.getElementById("2.8");
+    var j = document.getElementById("2.9");
+    var k = document.getElementById("2.10");
+
+    a.className = "selectNumberDiv";
+    b.className = "selectNumberDiv";
+    c.className = "selectNumberDiv";
+    d.className = "selectNumberDiv";
+    e.className = "selectNumberDiv";
+    f.className = "selectNumberDiv";
+    g.className = "selectNumberDiv";
+    h.className = "selectNumberDiv";
+    i.className = "selectNumberDiv";
+    j.className = "selectNumberDiv";
+    k.className = "selectNumberDiv";
+
+    if (selected.id == "2.0")
+        results[1] = 0;
+    if (selected.id == "2.1")
+        results[1] = 1;
+    if (selected.id == "2.2")
+        results[1] = 2;
+    if (selected.id == "2.3")
+        results[1] = 3;
+    if (selected.id == "2.4")
+        results[1] = 4;
+    if (selected.id == "2.5")
+        results[1] = 5;
+    if (selected.id == "2.6")
+        results[1] = 6;
+    if (selected.id == "2.7")
+        results[1] = 7;
+    if (selected.id == "2.8")
+        results[1] = 8;
+    if (selected.id == "2.9")
+        results[1] = 9;
+    if (selected.id == "2.10")
+        results[1] = 10;
+
+    selected.className = "selectedNumberDiv";
+}
+
+function selectNausea(selected)
+{
+    var a = document.getElementById("3.0");
+    var b = document.getElementById("3.1");
+    var c = document.getElementById("3.2");
+    var d = document.getElementById("3.3");
+    var e = document.getElementById("3.4"); 
+    var f = document.getElementById("3.5");
+    var g = document.getElementById("3.6");
+    var h = document.getElementById("3.7");
+    var i = document.getElementById("3.8");
+    var j = document.getElementById("3.9");
+    var k = document.getElementById("3.10");
+
+    a.className = "selectNumberDiv";
+    b.className = "selectNumberDiv";
+    c.className = "selectNumberDiv";
+    d.className = "selectNumberDiv";
+    e.className = "selectNumberDiv";
+    f.className = "selectNumberDiv";
+    g.className = "selectNumberDiv";
+    h.className = "selectNumberDiv";
+    i.className = "selectNumberDiv";
+    j.className = "selectNumberDiv";
+    k.className = "selectNumberDiv";
+
+    if (selected.id == "3.0")
+        results[2] = 0;
+    if (selected.id == "3.1")
+        results[2] = 1;
+    if (selected.id == "3.2")
+        results[2] = 2;
+    if (selected.id == "3.3")
+        results[2] = 3;
+    if (selected.id == "3.4")
+        results[2] = 4;
+    if (selected.id == "3.5")
+        results[2] = 5;
+    if (selected.id == "3.6")
+        results[2] = 6;
+    if (selected.id == "3.7")
+        results[2] = 7;
+    if (selected.id == "3.8")
+        results[2] = 8;
+    if (selected.id == "3.9")
+        results[2] = 9;
+    if (selected.id == "3.10")
+        results[2] = 10;
+
+    selected.className = "selectedNumberDiv";
+}
+
+function selectDepression(selected)
+{
+    var a = document.getElementById("4.0");
+    var b = document.getElementById("4.1");
+    var c = document.getElementById("4.2");
+    var d = document.getElementById("4.3");
+    var e = document.getElementById("4.4");
+    var f = document.getElementById("4.5");
+    var g = document.getElementById("4.6");
+    var h = document.getElementById("4.7");
+    var i = document.getElementById("4.8");
+    var j = document.getElementById("4.9");
+    var k = document.getElementById("4.10");
+
+    a.className = "selectNumberDiv";
+    b.className = "selectNumberDiv";
+    c.className = "selectNumberDiv";
+    d.className = "selectNumberDiv";
+    e.className = "selectNumberDiv";
+    f.className = "selectNumberDiv";
+    g.className = "selectNumberDiv";
+    h.className = "selectNumberDiv";
+    i.className = "selectNumberDiv";
+    j.className = "selectNumberDiv";
+    k.className = "selectNumberDiv";
+
+    if (selected.id == "4.0")
+        results[3] = 0;
+    if (selected.id == "4.1")
+        results[3] = 1;
+    if (selected.id == "4.2")
+        results[3] = 2;
+    if (selected.id == "4.3")
+        results[3] = 3;
+    if (selected.id == "4.4")
+        results[3] = 4;
+    if (selected.id == "4.5")
+        results[3] = 5;
+    if (selected.id == "4.6")
+        results[3] = 6;
+    if (selected.id == "4.7")
+        results[3] = 7;
+    if (selected.id == "4.8")
+        results[3] = 8;
+    if (selected.id == "4.9")
+        results[3] = 9;
+    if (selected.id == "4.10")
+        results[3] = 10;
+
+    selected.className = "selectedNumberDiv";
+}
+
+function selectAnxiety(selected)
+{
+    var a = document.getElementById("5.0");
+    var b = document.getElementById("5.1");
+    var c = document.getElementById("5.2");
+    var d = document.getElementById("5.3");
+    var e = document.getElementById("5.4");
+    var f = document.getElementById("5.5");
+    var g = document.getElementById("5.6");
+    var h = document.getElementById("5.7");
+    var i = document.getElementById("5.8");
+    var j = document.getElementById("5.9");
+    var k = document.getElementById("5.10");
+
+    a.className = "selectNumberDiv"
+    b.className = "selectNumberDiv";
+    c.className = "selectNumberDiv";
+    d.className = "selectNumberDiv";
+    e.className = "selectNumberDiv";
+    f.className = "selectNumberDiv";
+    g.className = "selectNumberDiv";
+    h.className = "selectNumberDiv";
+    i.className = "selectNumberDiv";
+    j.className = "selectNumberDiv";
+    k.className = "selectNumberDiv";
+
+    if (selected.id == "5.0")
+        results[4] = 0;
+    if (selected.id == "5.1")
+        results[4] = 1;
+    if (selected.id == "5.2")
+        results[4] = 2;
+    if (selected.id == "5.3")
+        results[4] = 3;
+    if (selected.id == "5.4")
+        results[4] = 4;
+    if (selected.id == "5.5")
+        results[4] = 5;
+    if (selected.id == "5.6")
+        results[4] = 6;
+    if (selected.id == "5.7")
+        results[4] = 7;
+    if (selected.id == "5.8")
+        results[4] = 8;
+    if (selected.id == "5.9")
+        results[4] = 9;
+    if (selected.id == "5.10")
+        results[4] = 10;
+
+    selected.className = "selectedNumberDiv";
+}
+
+
+function selectDrowsiness(selected)
+{
+    var a = document.getElementById("6.0");
+    var b = document.getElementById("6.1");
+    var c = document.getElementById("6.2");
+    var d = document.getElementById("6.3");
+    var e = document.getElementById("6.4");
+    var f = document.getElementById("6.5");
+    var g = document.getElementById("6.6");
+    var h = document.getElementById("6.7");
+    var i = document.getElementById("6.8");
+    var j = document.getElementById("6.9");
+    var k = document.getElementById("6.10");
+
+    a.className = "selectNumberDiv";
+    b.className = "selectNumberDiv";
+    c.className = "selectNumberDiv";
+    d.className = "selectNumberDiv";
+    e.className = "selectNumberDiv";
+    f.className = "selectNumberDiv";
+    g.className = "selectNumberDiv";
+    h.className = "selectNumberDiv";
+    i.className = "selectNumberDiv";
+    j.className = "selectNumberDiv";
+    k.className = "selectNumberDiv";
+
+    if (selected.id == "6.0")
+        results[5] = 0;
+    if (selected.id == "6.1")
+        results[5] = 1;
+    if (selected.id == "6.2")
+        results[5] = 2;
+    if (selected.id == "6.3")
+        results[5] = 3;
+    if (selected.id == "6.4")
+        results[5] = 4;
+    if (selected.id == "6.5")
+        results[5] = 5;
+    if (selected.id == "6.6")
+        results[5] = 6;
+    if (selected.id == "6.7")
+        results[5] = 7;
+    if (selected.id == "6.8")
+        results[5] = 8;
+    if (selected.id == "6.9")
+        results[5] = 9;
+    if (selected.id == "6.10")
+        results[5] = 10;
+
+    selected.className = "selectedNumberDiv";
+}
+
+function selectAppetite(selected)
+{
+    var a = document.getElementById("7.0");
+    var b = document.getElementById("7.1");
+    var c = document.getElementById("7.2");
+    var d = document.getElementById("7.3");
+    var e = document.getElementById("7.4");
+    var f = document.getElementById("7.5");
+    var g = document.getElementById("7.6");
+    var h = document.getElementById("7.7");
+    var i = document.getElementById("7.8");
+    var j = document.getElementById("7.9");
+    var k = document.getElementById("7.10");
+
+    a.className = "selectNumberDiv";
+    b.className = "selectNumberDiv";
+    c.className = "selectNumberDiv";
+    d.className = "selectNumberDiv";
+    e.className = "selectNumberDiv";
+    f.className = "selectNumberDiv";
+    g.className = "selectNumberDiv";
+    h.className = "selectNumberDiv";
+    i.className = "selectNumberDiv";
+    j.className = "selectNumberDiv";
+    k.className = "selectNumberDiv";
+
+    if (selected.id == "7.0")
+        results[6] = 0;
+    if (selected.id == "7.1")
+        results[6] = 1;
+    if (selected.id == "7.2")
+        results[6] = 2;
+    if (selected.id == "7.3")
+        results[6] = 3;
+    if (selected.id == "7.4")
+        results[6] = 4;
+    if (selected.id == "7.5")
+        results[6] = 5;
+    if (selected.id == "7.6")
+        results[6] = 6;
+    if (selected.id == "7.7")
+        results[6] = 7;
+    if (selected.id == "7.8")
+        results[6] = 8;
+    if (selected.id == "7.9")
+        results[6] = 9;
+    if (selected.id == "7.10")
+        results[6] = 10;    
+
+    selected.className = "selectedNumberDiv";
+}
+
+function selectWellbeing(selected)
+{
+    var a = document.getElementById("8.0");
+    var b = document.getElementById("8.1");
+    var c = document.getElementById("8.2");
+    var d = document.getElementById("8.3");
+    var e = document.getElementById("8.4");
+    var f = document.getElementById("8.5");
+    var g = document.getElementById("8.6");
+    var h = document.getElementById("8.7");
+    var i = document.getElementById("8.8");
+    var j = document.getElementById("8.9");
+    var k = document.getElementById("8.10");
+
+    a.className = "selectNumberDiv";
+    b.className = "selectNumberDiv";
+    c.className = "selectNumberDiv";
+    d.className = "selectNumberDiv";
+    e.className = "selectNumberDiv";
+    f.className = "selectNumberDiv";
+    g.className = "selectNumberDiv";
+    h.className = "selectNumberDiv";
+    i.className = "selectNumberDiv";
+    j.className = "selectNumberDiv";
+    k.className = "selectNumberDiv";
+
+    if (selected.id == "8.0")
+        results[7] = 0;
+    if (selected.id == "8.1")
+        results[7] = 1;
+    if (selected.id == "8.2")
+        results[7] = 2;
+    if (selected.id == "8.3")
+        results[7] = 3;
+    if (selected.id == "8.4")
+        results[7] = 4;
+    if (selected.id == "8.5")
+        results[7] = 5;
+    if (selected.id == "8.6")
+        results[7] = 6;
+    if (selected.id == "8.7")
+        results[7] = 7;
+    if (selected.id == "8.8")
+        results[7] = 8;
+    if (selected.id == "8.9")
+        results[7] = 9;
+    if (selected.id == "8.10")
+        results[7] = 10;
+
+    selected.className = "selectedNumberDiv";
+}
+
+function selectBreath(selected) 
+{
+    var a = document.getElementById("9.0");
+    var b = document.getElementById("9.1");
+    var c = document.getElementById("9.2");
+    var d = document.getElementById("9.3");
+    var e = document.getElementById("9.4");
+    var f = document.getElementById("9.5");
+    var g = document.getElementById("9.6");
+    var h = document.getElementById("9.7");
+    var i = document.getElementById("9.8");
+    var j = document.getElementById("9.9");
+    var k = document.getElementById("9.10");
+
+    a.className = "selectNumberDiv";
+    b.className = "selectNumberDiv";
+    c.className = "selectNumberDiv";
+    d.className = "selectNumberDiv";
+    e.className = "selectNumberDiv";
+    f.className = "selectNumberDiv";
+    g.className = "selectNumberDiv";
+    h.className = "selectNumberDiv";
+    i.className = "selectNumberDiv";
+    j.className = "selectNumberDiv";
+    k.className = "selectNumberDiv";
+
+    if (selected.id == "9.0")
+        results[8] = 0;
+    if (selected.id == "9.1")
+        results[8] = 1;
+    if (selected.id == "9.2")
+        results[8] = 2;
+    if (selected.id == "9.3")
+        results[8] = 3;
+    if (selected.id == "9.4")
+        results[8] = 4;
+    if (selected.id == "9.5")
+        results[8] = 5;
+    if (selected.id == "9.6")
+        results[8] = 6;
+    if (selected.id == "9.7")
+        results[8] = 7;
+    if (selected.id == "9.8")
+        results[8] = 8;
+    if (selected.id == "9.9")
+        results[8] = 9;
+    if (selected.id == "9.10")
+        results[8] = 10;
+
+    selected.className = "selectedNumberDiv";
+}
+
